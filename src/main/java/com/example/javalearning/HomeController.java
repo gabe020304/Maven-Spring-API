015fe949df522ccf593af7c4743ed0a10071b325
Gabriel Curran <gcurran2004@gmail.com>
2023-07-07 08:11:55 -0600

    Added second API call, improved the java code, reviewed methods/variables, review method access modifiers, upgraded json pulling technique with a readability upgrade with a new maven library.

--- src/main/java/com/example/javalearning/HomeController.java
+++ src/main/java/com/example/javalearning/HomeController.java
@@ -1,5 +1,8 @@
 package com.example.javalearning;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,14 +13,42 @@ public class HomeController {
 
     @GetMapping("/")
     public String index(Model model) {
-        ResponseEntity<String> responseEntity = Main.fetchWeatherData();
+        ResponseEntity<String> responseEntity = Main.getWeatherData();
         if (responseEntity.getStatusCode().is2xxSuccessful()) {
             String weatherData = responseEntity.getBody();
-            model.addAttribute("weatherData", weatherData);
+            String prettyWeatherData = getPrettyPrintedJson(weatherData);
+            model.addAttribute("weatherData", prettyWeatherData);
         } else {
             String errorMessage = "Failed to fetch weather data";
             model.addAttribute("errorMessage", errorMessage);
         }
         return "index";
     }
+
+    @GetMapping("/last-thirty-days")
+    public String lastThirtyDays(Model model) {
+        ResponseEntity<String> responseEntity = Main.getLastThirtyDaysWeatherData();
+        if (responseEntity.getStatusCode().is2xxSuccessful()) {
+            String weatherThirtyDaysData = responseEntity.getBody();
+            String prettyWeatherThirtyDaysData = getPrettyPrintedJson(weatherThirtyDaysData);
+            model.addAttribute("weatherThirtyDaysData", prettyWeatherThirtyDaysData);
+        } else {
+            String errorMessage = "Failed to fetch weather data";
+            model.addAttribute("errorMessage", errorMessage);
+        }
+        return "last-thirty-days";
+    }
+
+    private String getPrettyPrintedJson(String jsonData) {
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
+            ObjectWriter writer = objectMapper.writerWithDefaultPrettyPrinter();
+            return writer.writeValueAsString(jsonData);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return jsonData;
+        }
+    }
 }
+
--- src/main/java/com/example/javalearning/Main.java
+++ src/main/java/com/example/javalearning/Main.java
@@ -1,27 +1,28 @@
 package com.example.javalearning;
 
-        import org.springframework.boot.SpringApplication;
-        import org.springframework.boot.autoconfigure.SpringBootApplication;
-        import org.springframework.http.ResponseEntity;
-        import org.springframework.web.client.RestTemplate;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class Main {
 
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
-        ResponseEntity<String> responseEntity = fetchWeatherData();
-        displayWeatherInfo(responseEntity.getBody());
     }
 
-    public static ResponseEntity<String> fetchWeatherData() {
-        return getWeatherData();
-    }
-
-    private static ResponseEntity<String> getWeatherData() {
+    static ResponseEntity<String> getWeatherData() {
         RestTemplate restTemplate = new RestTemplate();
-        String apiKey = "98b88358e13a619f030a424cfc92672b";
+        String apiKey = "API Key";
         String apiUrl = "https://api.openweathermap.org/data/2.5/weather?zip=84107,us&appid=" + apiKey;
+        return restTemplate.getForEntity(apiUrl, String.class);
+    }
+
+    static ResponseEntity<String> getLastThirtyDaysWeatherData() {
+        RestTemplate restTemplate = new RestTemplate();
+        String apiKey = "API Key";
+        String apiUrl = "https://api.openweathermap.org/data/2.5/forecast?zip=84107,us&appid=" + apiKey;
         return restTemplate.getForEntity(apiUrl, String.class);
     }
 
